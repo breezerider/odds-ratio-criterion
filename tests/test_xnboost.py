abe7d7a49537f73998fcaded285103e1121bf0cc
Olekandr Ostrenko <olekandr.ostrenko@gmail.com>
2023-02-12 13:14:45 +0100

    New tests for XNBoostClassifier
    * use 'odds_ratio' splitting criterion
    * 4-variable XOR function test case
    * 3-variable AND-XOR function:
      - test fitting multiple weak learners
      - test exception message when the first learner performs worse than random guessing

--- tests/test_xnboost.py
+++ tests/test_xnboost.py
@@ -1,3 +1,4 @@
+import unittest
 
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
@@ -5,15 +6,74 @@ from sklearn.tree import DecisionTreeClassifier
 from odds_ratio_criterion import XNBoostClassifier
 
 
-def test_xnboost_classifier():
-    X_train = np.array([0, 1]).reshape(-1, 1)
-    y_train = np.array([1, 0]).T
+class XNBoostTestCase(unittest.TestCase):
+    X_xor = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 1, 0, 0],
+        [0, 1, 0, 1],
+        [0, 1, 1, 0],
+        [0, 1, 1, 1],
+        [1, 0, 0, 0],
+        [1, 0, 0, 1],
+        [1, 0, 1, 0],
+        [1, 0, 1, 1],
+        [1, 1, 0, 0],
+        [1, 1, 0, 1],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1],
+    ]
 
-    X_test = np.array([1, 0]).reshape(-1, 1)
-    y_test = np.array([0, 1]).T
+    y_xor = [0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0]
 
-    for criterion in ["gini", "entropy", "log_loss", "odds_ratio"]:
-        model = XNBoostClassifier(DecisionTreeClassifier(max_depth=1, criterion=criterion), n_estimators=1, loss_exponent=4, random_state=42)
-        model.fit(X_train, y_train)
+    X_fcn = [
+        [0, 0, 0],
+        [0, 0, 1],
+        [0, 1, 0],
+        [0, 1, 1],
+        [1, 0, 0],
+        [1, 0, 1],
+        [1, 1, 0],
+        [1, 1, 1],
+    ]
 
-        assert np.all(model.predict(X_test) == y_test)
+    y_fcn = [0, 1, 0, 1, 0, 1, 1, 0]
+
+    def test_xnboost_classifier_learning_xor_fcn(self):
+        X = np.array(self.X_xor)
+        y = np.array(self.y_xor).T
+
+        model = XNBoostClassifier(
+            estimator=DecisionTreeClassifier(max_depth=4, criterion="odds_ratio"), n_estimators=1, loss_exponent=4, random_state=42
+        )
+        model.fit(X, y)
+
+        assert np.all(model.predict(X) == y)
+
+    def test_xnboost_classifier_learning_fcn(self):
+        X = np.array(self.X_fcn)
+        y = np.array(self.y_fcn).T
+
+        model = XNBoostClassifier(
+            estimator=DecisionTreeClassifier(max_depth=2, criterion="odds_ratio"), n_estimators=3, loss_exponent=4, random_state=42
+        )
+
+        model.fit(X, y)
+
+        assert np.all(model.predict(X) == y)
+
+    def test_xnboost_classifier_learning_fail(self):
+        X = np.array(self.X_xor)
+        y = np.array(self.y_xor).T
+
+        model = XNBoostClassifier(
+            estimator=DecisionTreeClassifier(max_depth=1, criterion="odds_ratio"), n_estimators=1, loss_exponent=4, random_state=42
+        )
+        with self.assertRaises(ValueError) as context:
+            model.fit(X, y)
+
+        self.assertTrue(
+            'BaseClassifier in XNBoostClassifier ensemble is worse than random, ensemble can not be fit.' in str(context.exception)
+        )
